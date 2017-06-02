ec281a5ca0e4599cd0f17d35d3937aea3ce75ac1
Andrew Hill <contact@andrewthomashill.co.uk>
2017-06-02 18:10:59 +0100

    Add Favicons

--- header.php
+++ header.php
@@ -17,6 +17,26 @@
     <meta name="viewport" content="width=device-width">
     <?php echo file_get_contents(get_stylesheet_directory_uri() .'/assets/js/loader.html'); ?>
     <link href="https://fonts.googleapis.com/css?family=Fjalla+One%7COpen+Sans" rel="stylesheet">
+    <link rel="apple-touch-icon-precomposed" sizes="57x57" href="https://crookedbeverageco.com/favicons/apple-touch-icon-57x57.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="https://crookedbeverageco.com/favicons/apple-touch-icon-114x114.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="https://crookedbeverageco.com/favicons/apple-touch-icon-72x72.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="https://crookedbeverageco.com/favicons/apple-touch-icon-144x144.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="60x60" href="https://crookedbeverageco.com/favicons/apple-touch-icon-60x60.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="120x120" href="https://crookedbeverageco.com/favicons/apple-touch-icon-120x120.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="76x76" href="https://crookedbeverageco.com/favicons/apple-touch-icon-76x76.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="152x152" href="https://crookedbeverageco.com/favicons/apple-touch-icon-152x152.png" />
+    <link rel="icon" type="image/png" href="https://crookedbeverageco.com/favicons/favicon-196x196.png" sizes="196x196" />
+    <link rel="icon" type="image/png" href="https://crookedbeverageco.com/favicons/favicon-96x96.png" sizes="96x96" />
+    <link rel="icon" type="image/png" href="https://crookedbeverageco.com/favicons/favicon-32x32.png" sizes="32x32" />
+    <link rel="icon" type="image/png" href="https://crookedbeverageco.com/favicons/favicon-16x16.png" sizes="16x16" />
+    <link rel="icon" type="image/png" href="https://crookedbeverageco.com/favicons/favicon-128.png" sizes="128x128" />
+    <meta name="application-name" content="Crooked Beverage Co"/>
+    <meta name="msapplication-TileColor" content="#BLACK" />
+    <meta name="msapplication-TileImage" content="https://crookedbeverageco.com/favicons/mstile-144x144.png" />
+    <meta name="msapplication-square70x70logo" content="https://crookedbeverageco.com/favicons/mstile-70x70.png" />
+    <meta name="msapplication-square150x150logo" content="https://crookedbeverageco.com/favicons/mstile-150x150.png" />
+    <meta name="msapplication-wide310x150logo" content="https://crookedbeverageco.com/favicons/mstile-310x150.png" />
+    <meta name="msapplication-square310x310logo" content="https://crookedbeverageco.com/favicons/mstile-310x310.png" />
     <?php wp_head(); ?>
 </head>
 <body class="<?php echo $world; ?> sunrise daytime">
