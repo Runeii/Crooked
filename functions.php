1a573bc23ba48b79527094b1957ae4712663ba26
Andrew Hill <contact@andrewthomashill.co.uk>
2017-06-07 15:32:03 +0100

    Update resource query string versions

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 function crooked_scripts_styles() {
-    wp_enqueue_style( 'styles', get_stylesheet_directory_uri() . '/assets/css/style-min.css', null, '1', 'all' );
-    wp_register_script( 'main', get_template_directory_uri() . '/assets/js/scripts-min.js', null, '1', true);
+    wp_enqueue_style( 'styles', get_stylesheet_directory_uri() . '/assets/css/style-min.css', null, '1.0.1', 'all' );
+    wp_register_script( 'main', get_template_directory_uri() . '/assets/js/scripts-min.js', null, '1.0.1', true);
     // Localize the script with new data
     $translation_array = array(
     	'template_directory' => str_replace(get_home_url(), '', get_template_directory_uri()),
